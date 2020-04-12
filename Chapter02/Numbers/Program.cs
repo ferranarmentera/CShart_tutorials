39583ee927af2803ed68681922b0af25dd530b9d
ferranarmentera <ferran.armentera@gmail.com>
2020-04-12 09:36:02 +0200

    primer commit

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
+if (a + b == 0.3)
+{
+Console.WriteLine($"{a} + {b} equals 0.3");
+}
+else
+{
+Console.WriteLine($"{a} + {b} does NOT equal 0.3");
+}
+        }
+    }
+}
