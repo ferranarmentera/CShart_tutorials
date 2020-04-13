dfe2ca079db5425f3e04d3a034dafb69c8e84374
Ferran <49804072+ferranarmentera@users.noreply.github.com>
2020-04-13 15:28:25 +0200

    primer commit

---
+++ Chapter02/Exercise02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
